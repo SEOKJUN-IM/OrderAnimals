210693d6977682c9af41c558cf0d5c4c5715f77b
Seokjun Im <carnivorous5@gmail.com>
2025-06-11 13:02:28 +0900

    [Chore] Slot, Animal 프리팹 추가

---
+++ Assets/006.Scripts/Entity/Animal.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Animal : MonoBehaviour, IPoolable
+{
+    private ObjectPool<GameObject> pool;
+    public ObjectPool<GameObject> Pool { get => pool; set => pool = value; }
+
+    public GameObject self => this.gameObject;    
+}
