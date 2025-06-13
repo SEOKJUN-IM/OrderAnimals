407ae16d4027bc995d443c220c6d1e3721830665
Seokjun Im <carnivorous5@gmail.com>
2025-06-13 16:01:34 +0900

    [Feat] 노말모드, 블라인드모드 추가, 슬롯 변경

--- Assets/006.Scripts/Entity/Slot.cs
+++ Assets/006.Scripts/Entity/Slot.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -6,10 +7,23 @@ public class Slot : MonoBehaviour, IPoolable
     private ObjectPool<GameObject> pool;
     public ObjectPool<GameObject> Pool { get => pool; set => pool = value; }
 
-    public GameObject self => this.gameObject;
-
-    [Header("Type")]
-    public OwnerType ownerType; // 소유자 타입 (컴퓨터 또는 플레이어)
+    public GameObject self => this.gameObject;    
 
     public int index;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Animal curAnimal;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Animal animal = collision.GetComponent<Animal>();
+        curAnimal = animal;
+
+        if (curAnimal == null) return;
+
+        if (curAnimal.index == index) spriteRenderer.color = Color.green;
+        else if (Mathf.Abs(curAnimal.index - index) == 1) spriteRenderer.color = Color.orange;
+        else spriteRenderer.color = Color.red;
+    }
 }
--- Assets/006.Scripts/Managers/GameManager.cs
+++ Assets/006.Scripts/Managers/GameManager.cs
@@ -8,24 +8,36 @@ using UnityEngine.AddressableAssets;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameMode
+    {
+        Normal,
+        Blind
+    }
+
+    public GameMode curGameMode;
+
     [SerializeField] private AssetReference mainScene; // 메인 씬 에셋 레퍼런스    
 
     [Header("게임 설정")]
-    [Range(4, 12)] public int slotCount;
+    [Range(4, 12)] public int animalCount;
 
     [Header("게임 UI")]
     [SerializeField] private TextMeshProUGUI warningText;
     [SerializeField] private GameObject selectedMarks;
     [SerializeField] private GameObject leftMark;
     [SerializeField] private GameObject rightMark;
+    [SerializeField] private GameObject switchTitleText;
+    [SerializeField] private TextMeshProUGUI switchCountText;
+    private int switchCount;
 
     // 애니멀 스프라이트 랜덤 할당
     [SerializeField] private AssetReference[] animalSprites; // 애니멀 스프라이트들
     private AssetReference[] randomSprites; // 랜덤으로 선택된 애니멀 스프라이트들
     private Sprite[] animalSpritesArray; // 애니멀 스프라이트 배열
 
-    // 플레이어 애니멀
+    // 플레이어 애니멀 & 슬롯
     private Animal[] playerAnimals;
+    public Slot[] playerSlots;
 
     private Animal firstAnimal;
     private Animal secondAnimal;
@@ -61,45 +73,60 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void Initialize()
     {
+        switchCount = 0;
+        switchCountText.text = switchCount.ToString();
+
+        switchTitleText.SetActive(true);
+        switchCountText.gameObject.SetActive(true);
+
         SetSlotsAndAnimals();        
     }
 
     // 슬롯 켜주고 컴퓨터, 플레이어 구분, 위치 선정
     private void SetSlotsAndAnimals()
     {
-        // 플레이어 애니멀 담아둘 배열 생성
-        playerAnimals = new Animal[slotCount];
+        // 플레이어 애니멀 & 슬롯 담아둘 배열 생성
+        playerAnimals = new Animal[animalCount];
+        playerSlots = new Slot[animalCount];
 
         // 애니멀 스프라이트 배열 생성
         MakeAnimalSpritesArray();
 
-        for (int i = 0; i < slotCount * 2; i++)
+        for (int i = 0; i < animalCount * 2; i++)
         {
-            Slot slot = ObjectPoolManager.Instance.Get("Slot").GetComponent<Slot>();
-            Animal animal = ObjectPoolManager.Instance.Get("Animal").GetComponent<Animal>();
+            Animal animal = ObjectPoolManager.Instance.Get("Animal").GetComponent<Animal>();            
 
-            slot.ownerType = (i % 2 != 0) ? OwnerType.Computer : OwnerType.Player; // 홀수 인덱스는 컴퓨터, 짝수 인덱스는 플레이어
-            animal.ownerType = slot.ownerType; // 슬롯과 동일한 소유자 타입 설정
+            animal.ownerType = (i % 2 != 0) ? OwnerType.Computer : OwnerType.Player; // 홀수 인덱스는 컴퓨터, 짝수 인덱스는 플레이어            
 
-            if (i % 2 != 0) // 컴퓨터 애니멀 : 홀수 인덱스 yPosition은 3f
+            if (i % 2 != 0) // 컴퓨터 : 홀수 인덱스 yPosition은 3f
             {
-                slot.index = (i - 1) / 2; // 홀수 인덱스는 0부터 시작하는 인덱스
-                animal.index = slot.index; // 동물 인덱스도 동일하게 설정
+                animal.index = (i - 1) / 2; // 홀수 인덱스는 0부터 시작하는 인덱스                
 
                 SetAnimalSprites(animal); // 애니멀 스프라이트 설정
-                SetSlotPosition(slot, 3f);
+                SetAnimalPosition(animal, 3f);
+
+                if (curGameMode == GameMode.Blind) animal.gameObject.SetActive(false);
             }
             else // 짝수 인덱스 yPosition은 -2f
             {
-                slot.index = i / 2; // 플레이어 애니멀 : 짝수 인덱스는 0부터 시작하는 인덱스
-                animal.index = slot.index; // 동물 인덱스도 동일하게 설정
+                animal.index = i / 2; // 플레이어 : 짝수 인덱스는 0부터 시작하는 인덱스                
 
                 SetAnimalSprites(animal); // 애니멀 스프라이트 설정
-                SetSlotPosition(slot, -2f);
+                SetAnimalPosition(animal, -2f);
                 playerAnimals[animal.index] = animal;
             }
+        }
 
-            animal.transform.position = slot.transform.position; // 동물 위치를 슬롯 위치로 설정            
+        for (int i = 0; i < playerAnimals.Length; i++)
+        {
+            Slot slot = ObjectPoolManager.Instance.Get("Slot").GetComponent<Slot>();
+
+            slot.index = playerAnimals[i].index;
+            slot.transform.position = playerAnimals[i].transform.position;
+            playerSlots[slot.index] = slot;
+
+            if (curGameMode == GameMode.Normal) slot.spriteRenderer.gameObject.SetActive(false);
+            else slot.spriteRenderer.gameObject.transform.position += Vector3.up * 5f;
         }
 
         ReleaseSpriteAssets(); // 사용한 스프라이트 에셋 해제
@@ -107,37 +134,37 @@ public class GameManager : MonoBehaviour
         RandomPlayerAnimals(); // 플레이어 애니멀 랜덤 배치
     }
 
-    // 슬롯 배치
-    private void SetSlotPosition(Slot slot, float yPosition)
+    // 애니멀 배치
+    private void SetAnimalPosition(Animal animal, float yPosition)
     {
-        float[] slotPositions = new float[slotCount];
+        float[] animalPositions = new float[animalCount];
 
         // 슬롯 수 홀수일 때
-        if (slotCount % 2 != 0)
+        if (animalCount % 2 != 0)
         {
-            slotPositions[(slotCount - 1) / 2] = 0f; // 중앙 슬롯 위치 설정
-            for (int i = 1; i <= slotCount / 2; i++)
+            animalPositions[(animalCount - 1) / 2] = 0f; // 중앙 슬롯 위치 설정
+            for (int i = 1; i <= animalCount / 2; i++)
             {
-                slotPositions[(slotCount - 1) / 2 - i] = -i * 1.2f; // 왼쪽 슬롯 위치 설정
-                slotPositions[(slotCount - 1) / 2 + i] = i * 1.2f; // 오른쪽 슬롯 위치 설정
+                animalPositions[(animalCount - 1) / 2 - i] = -i * 1.2f; // 왼쪽 슬롯 위치 설정
+                animalPositions[(animalCount - 1) / 2 + i] = i * 1.2f; // 오른쪽 슬롯 위치 설정
             }
         }
         else // 슬롯 수 짝수일 때
         {
-            for (int i = 1; i <= slotCount / 2; i++)
+            for (int i = 1; i <= animalCount / 2; i++)
             {
-                slotPositions[slotCount / 2 - i] = -i * 1.2f + 0.6f; // 왼쪽 슬롯 위치 설정
-                slotPositions[slotCount / 2 + i - 1] = (i - 1) * 1.2f + 0.6f; // 오른쪽 슬롯 위치 설정
+                animalPositions[animalCount / 2 - i] = -i * 1.2f + 0.6f; // 왼쪽 슬롯 위치 설정
+                animalPositions[animalCount / 2 + i - 1] = (i - 1) * 1.2f + 0.6f; // 오른쪽 슬롯 위치 설정
             }
         }
 
-        slot.transform.position = new Vector2(slotPositions[slot.index], yPosition);
+        animal.transform.position = new Vector2(animalPositions[animal.index], yPosition);
     }
 
-    // 애니멀 스프라이트 에셋에서 랜덤으로 가져와 slotCount 크기 배열 생성
+    // 애니멀 스프라이트 에셋에서 랜덤으로 가져와 animalCount 크기 배열 생성
     private void MakeAnimalSpritesArray()
     {
-        randomSprites = animalSprites.OrderBy(x => UnityEngine.Random.value).Take(slotCount).ToArray();
+        randomSprites = animalSprites.OrderBy(x => UnityEngine.Random.value).Take(animalCount).ToArray();
 
         animalSpritesArray = new Sprite[randomSprites.Length];
 
@@ -256,15 +283,18 @@ public class GameManager : MonoBehaviour
         int firstIndex = Array.IndexOf(playerAnimals, firstAnimal);
         int secondIndex = Array.IndexOf(playerAnimals, secondAnimal);
         playerAnimals[firstIndex] = secondAnimal;
-        playerAnimals[secondIndex] = firstAnimal;        
+        playerAnimals[secondIndex] = firstAnimal;
 
         // 첫 번째 애니멀과 두 번째 애니멀을 초기화
         firstAnimal.animator.SetBool("Selected", false);
         secondAnimal.animator.SetBool("Selected", false);
-        
+
         firstAnimal = null;
         secondAnimal = null;
         canSwitchAnimals.Clear();
+
+        switchCount++;
+        switchCountText.text = switchCount.ToString();
     }
 
     public void CancelSelect()
